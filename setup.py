3bda893beb3afa639aace9423ec819129d55c0f3
Ionut Filip <ionut.philip@gmail.com>
2019-01-24 13:10:34 +0200

    Package creation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mailustart',
+      version='0.1',
+      description='Common functions used by Mailu\'s start.py',
+      url='https://github.com/Mailu/MailuStart',
+      author='Ionut Filip',
+      author_email='ionut.filip@usrpro.com',
+      license='MIT',
+      packages=['mailustart'],
+      zip_safe=False)
